14964c4a69c86962a98c3565b1c83a5dda910509
Jon Taylor <95875505+JWT890@users.noreply.github.com>
2024-01-12 15:12:53 -0600

    Update scope.sh

--- rmf/prepare/scope.sh
+++ rmf/prepare/scope.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# displays the name of the application
 echo "System Scope Definition Script"
+# reads and takes the user input for system name
 read -p "Enter system name: " system_name
+# reads and takes the user input for system description
 read -p "Enter system description: " system_description
+# reads and takes the user input for the key elements
 read -p "Enter key elements: " key_elements
 
+# displays the user input for the system name and appends it to the txt file
 echo "System Name: $system_name" >> systemscope.txt
+# displays the user input for the system description and appends it to the txt file
 echo "System Description: $system_description" >> systemscope.txt
+# displays the user input for the key elements and appends it to the txt file
 echo "Key components: $key_elements" >> systemscope.txt
 
 
