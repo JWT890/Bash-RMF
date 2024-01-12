a701714beb66c66afa9f12c130b2fb5cb452ea54
Jon Taylor <95875505+JWT890@users.noreply.github.com>
2024-01-12 15:07:14 -0600

    Update risk_assess_plan.sh

--- rmf/prepare/risk_assess_plan.sh
+++ rmf/prepare/risk_assess_plan.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# prints the name of the application
 echo "Risk Assessment Planning Tool"
 
+# asks and reads the user input for scope, objectives, methodology, and resources
 read -p "Enter assessment scope: " assessment_scope
 read -p "Enter assessment objectives: " assessment_objectives
 read -p "Enter assessment methodology: " assessment_methodology
 read -p "Enter required resources: " assessment_resources
 
+# creates a txt file for the Risk Assessment Plan from the inputs
+# displays the inputs for each one
 cat <<EOL > "Risk_Assessment_Plan.txt"
 Assessment Scope: $assessment_scope
 Assessment Objectives: $assessment_objectives
@@ -15,4 +19,5 @@ Required Resources: $assessment_resources
 
 EOL
 
+# prints the plan has been created in the Risk Assessment Plan
 echo "Risk assessment plan created: Risk_Assessment_Plan.txt"
