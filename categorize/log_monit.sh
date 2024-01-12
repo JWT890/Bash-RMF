7ea242b14cc687e3e2d29af774e4fdf9a2341010
Jon Taylor <95875505+JWT890@users.noreply.github.com>
2024-01-12 15:04:08 -0600

    Update log_monit.sh

--- categorize/log_monit.sh
+++ categorize/log_monit.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# prints the name of the application
 echo "Logging and Monitor Configuration"
 echo "---------------------"
 
+# checks to see if syslog is active or not
 syslog_status=$(systemctl is-active rsyslog)
+# prints the status of syslog
 echo "Syslog Status: $syslog_status"
 
+# prints the configuration of the .conf file
 echo "Logrotate Configuration:"
 cat /etc/logrotate.conf
