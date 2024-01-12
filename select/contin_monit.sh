383bbf3f464a054835fe2bbb8b6607445e1274c5
Jon Taylor <95875505+JWT890@users.noreply.github.com>
2024-01-12 15:16:51 -0600

    Update contin_monit.sh

--- select/contin_monit.sh
+++ select/contin_monit.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# function for getting user input for the script
 function get_input() {
+	# prints the name of the application
 	echo "Continous Monitoring Strategy Script"
 	echo "------------------------------------"
+ 	# asks the user for the system name
 	read -p "Enter the system name: " system_name
+ 	# asks the user for the monitoring frequency
 	read -p "Enter the monitoring frequency (daily, weekly, monthly): " monitoring_frequency
+ 	# asks the user for the tools or methods
 	read -p "Enter the monitoring tools or methods (comma-separated): " monitoring_tools
+ 	# asks the user for the notifiication preference
 	read -p "Enter the notification mechanism in case of anomolies: " notification_mechanism
 }
 
+# function for generating the monitoring strategy
 function generate_strategy() {
+	# creates the txt file and displays the user input in respective categories
 	cat << EOF > "Contin_Monit_Strat.txt"
 Continous Monitoring Strategy for $system_name
 ---------------------------------------------
@@ -27,7 +35,10 @@ This strategy will help in the early detection of security issues and ensure tim
 EOF
 }
 
+# runs the get_input function
 get_input
+# runs the generate_strategy function
 generate_strategy
 
+# prints the file as been generated and created
 echo "Continous Monitoring Strategy generated and created: Contin_Monitor_Strat.txt"
